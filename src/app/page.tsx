3a0e335cfb6bebd8f63a4ee8c33d8ad397b54179
Rodolpho Reis <rodolphoreis@live.com>
2024-10-12 19:42:34 +0100

    feat(home): add VelocityScroll component to Home page with dynamic text effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import { Header } from "@/components/Header";
+import { VelocityScroll } from "@/components/ui/scroll-based-velocity";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <h1>Home</h1>
+      <VelocityScroll
+        text="devMoney"
+        default_velocity={5}
+        className="text-3xl md:text-5xl opacity-5 flex gap-4 my-1 font-semibold"
+      />
     </div>
   );
 }
