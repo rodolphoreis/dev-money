a507d6a550678e5000fe89fde63b832b57967f48
Rodolpho Reis <rodolphoreis@live.com>
2024-10-12 19:42:01 +0100

    refactor(ui): remove px-2 from className in font setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased px-2`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
         <ThemeProvider
           attribute="class"
