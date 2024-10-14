7abd89e9c6454a828d3178946e660403c2de3b8e
Rodolpho Reis <rodolphoreis@live.com>
2024-10-14 16:38:04 +0100

    feat: add static TableMoney component for mobile, cards will be updated to fetch data from API

--- src/components/TableMoney.tsx
+++ src/components/TableMoney.tsx
@@ -7,10 +7,19 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+
 export function TableMoney() {
   return (
     <section className="w-full   content-center mt-4 md:mt-6 mx-auto ">
-      <div className="w-full max-w-[1120px] items-center justify-around md:justify-between flex  mx-auto ">
+      <div className="hidden md:flex w-full max-w-[1120px] items-center justify-around md:justify-between   mx-auto ">
         <Table>
           <TableCaption className="mt-20">
             Uma lista dos seus gastos recentes.
@@ -64,6 +73,64 @@ export function TableMoney() {
             </TableRow>
           </TableBody>
         </Table>
+      </div>
+      <div className="md:hidden w-full max-w-[1120px] items-center  flex flex-col gap-4 mx-auto ">
+        <Card className="w-[95%] ">
+          <CardHeader>
+            <CardTitle> Desenvolvimento de site</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-green-600">2000.00€</p>
+          </CardContent>
+          <div className="flex justify-between px-6 mb-5">
+            <CardDescription className="flex items-center gap-2">
+              <Image
+                src="/saleIcon.svg"
+                alt="icone de dinheiro"
+                width={20}
+                height={20}
+              ></Image>
+              <span>Venda</span>
+            </CardDescription>
+            <CardDescription className="flex items-center gap-2">
+              <Image
+                src="/calendar.svg"
+                alt="icone de calendario"
+                width={20}
+                height={20}
+              ></Image>
+              <span>01/10/2024</span>
+            </CardDescription>
+          </div>
+        </Card>
+        <Card className="w-[95%] ">
+          <CardHeader>
+            <CardTitle> Hamburguer</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-red-600">- 57.00€</p>
+          </CardContent>
+          <div className="flex justify-between px-6 mb-5">
+            <CardDescription className="flex items-center gap-2">
+              <Image
+                src="/saleIcon.svg"
+                alt="icone de dinheiro"
+                width={20}
+                height={20}
+              ></Image>
+              <span>Alimentação</span>
+            </CardDescription>
+            <CardDescription className="flex items-center gap-2">
+              <Image
+                src="/calendar.svg"
+                alt="icone de calendario"
+                width={20}
+                height={20}
+              ></Image>
+              <span>12/10/2024</span>
+            </CardDescription>
+          </div>
+        </Card>
       </div>
     </section>
   );
