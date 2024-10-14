851fab52c2d44ccf4a0023b309bcf521cb84af32
Rodolpho Reis <rodolphoreis@live.com>
2024-10-14 16:35:36 +0100

    refactor: replace header code with NewTransactionModal component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
 import { RiMoneyEuroCircleFill } from "react-icons/ri";
-import { BsDatabaseAdd } from "react-icons/bs";
+
 import { ModeToggle } from "./ModeToggle";
 import TypingAnimation from "./ui/typing-animation";
-import ShinyButton from "./ui/shiny-button";
+
+import { NewTransactionModal } from "./NewTransactionModal";
 
 export function Header() {
   return (
@@ -15,19 +15,9 @@ export function Header() {
             <TypingAnimation text="devMoney" className="text-3xl" />
           </span>
         </div>
-        <div className=" flex gap-1 items-center">
-          <Link
-            href={""}
-            className="bg-background text-foreground text-sm py-1 px-3 content-center  hover:text-secondary-foreground"
-          >
-            <ShinyButton className="hidden md:flex md:gap-2 items-center">
-              Nova transação
-            </ShinyButton>
+        <div className=" flex gap-1 md:gap-3 items-center">
+          <NewTransactionModal />
 
-            <ShinyButton className="md:hidden p-3">
-              <BsDatabaseAdd />
-            </ShinyButton>
-          </Link>
           <ModeToggle />
         </div>
       </div>
