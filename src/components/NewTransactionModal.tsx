efcca2342386bc68f2f05f32c83a89c77a944983
Rodolpho Reis <rodolphoreis@live.com>
2024-10-15 17:37:05 +0100

    feat: add button selection and color change for transaction type

--- src/components/NewTransactionModal.tsx
+++ src/components/NewTransactionModal.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Dialog,
   DialogContent,
@@ -11,8 +12,16 @@ import { BsDatabaseAdd } from "react-icons/bs";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { FaRegArrowAltCircleDown, FaRegArrowAltCircleUp } from "react-icons/fa";
+import { useState } from "react";
 
 export function NewTransactionModal() {
+  const [selectedValue, setSelectedValue] = useState("entrada");
+
+  function handlesetSelectedValue(value: string) {
+    setSelectedValue(value);
+    console.log(value);
+  }
+
   return (
     <Dialog>
       <DialogTrigger>
@@ -23,31 +32,51 @@ export function NewTransactionModal() {
           <BsDatabaseAdd />
         </div>
       </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Você tem certeza absoluta?</DialogTitle>
-        </DialogHeader>
-        <DialogDescription>
-          <Input type="text" placeholder="Descrição" className="mb-3" />
-          <Input type="text" placeholder="Preço" className="mb-3" />
-          <Input type="text" placeholder="Categoria" className="mb-3" />
+      <form action="">
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Você tem certeza absoluta?</DialogTitle>
+          </DialogHeader>
+          <DialogDescription>
+            <Input type="text" placeholder="Descrição" className="mb-3" />
+            <Input type="text" placeholder="Preço" className="mb-3" />
+            <Input type="text" placeholder="Categoria" className="mb-3" />
 
-          <div className="flex gap-3 mb-5">
-            <Button type="button" className="flex gap-2 mt-3 w-full">
-              <FaRegArrowAltCircleUp size={18} color="green" />
-              <span className="text-xs ">Entrada</span>{" "}
-            </Button>
-            <Button type="button" className="flex gap-2 mt-3 w-full">
-              <FaRegArrowAltCircleDown size={18} color="red" />
-              <span className="text-xs ">Saída</span>{" "}
-            </Button>
-          </div>
+            <div className="flex gap-3 mb-5">
+              <button
+                type="button"
+                className={`flex gap-2 h-10 mt-3 bg-zinc-900 rounded-lg w-full items-center justify-center border-4 ${
+                  selectedValue === "entrada"
+                    ? "bg-green-800 dark:bg-green-900 border-green-500"
+                    : "border-transparent"
+                }`}
+                onClick={() => handlesetSelectedValue("entrada")}
+              >
+                <FaRegArrowAltCircleUp size={18} color="green" />
+                <span className="text-xs font-bold text-white">
+                  Entrada
+                </span>{" "}
+              </button>
+              <button
+                type="button"
+                className={`flex gap-2 h-10 mt-3 bg-zinc-900 rounded-lg w-full items-center justify-center border-4 ${
+                  selectedValue === "saida"
+                    ? "bg-red-600 dark:bg-red-800 border-red-500"
+                    : "border-transparent"
+                }`}
+                onClick={() => handlesetSelectedValue("saida")}
+              >
+                <FaRegArrowAltCircleDown size={18} color="red" />
+                <span className="text-xs font-bold text-white">Saída</span>{" "}
+              </button>
+            </div>
 
-          <Button type="submit" className="flex gap-2 mt-3 w-full">
-            <span className="text-xs  ">Cadastrar</span>{" "}
-          </Button>
-        </DialogDescription>
-      </DialogContent>
+            <Button type="submit" className="flex gap-2 mt-3 w-full">
+              <span className="text-xs  ">Cadastrar</span>{" "}
+            </Button>
+          </DialogDescription>
+        </DialogContent>
+      </form>
     </Dialog>
   );
 }
