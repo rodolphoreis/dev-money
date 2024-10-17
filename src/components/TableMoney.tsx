e7a31d4f40a1a67db8c3b20fb572fa22c4164dc4
Rodolpho Reis <rodolphoreis@live.com>
2024-10-17 17:22:00 +0100

    feat: add delete transaction functionality with API integration

--- src/components/TableMoney.tsx
+++ src/components/TableMoney.tsx
@@ -15,12 +15,29 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+
+import { FaRegTrashAlt } from "react-icons/fa";
+
 import Image from "next/image";
 import { useContext } from "react";
 import { DevMoneyContext } from "../../context/devMoneyContext";
+import axios from "axios";
+
+function handleDelete(id: string) {
+  axios
+    .delete(`http://localhost:3001/transactions/${id}`)
+    .then((response) => {
+      if (response.status === 200) {
+        window.location.reload();
+      }
+    })
+    .catch((error) => {
+      console.error("Erro ao excluir a transação:", error);
+    });
+}
 
 export function TableMoney() {
-  const { transitions } = useContext(DevMoneyContext);
+  const { transactions } = useContext(DevMoneyContext);
 
   return (
     <section className="w-full   content-center mt-4 md:mt-6 mx-auto ">
@@ -40,32 +57,42 @@ export function TableMoney() {
               <TableHead className="md:w-[20%] text-xs md:text-sm text-zinc-500">
                 Categoria
               </TableHead>
-              <TableHead className="md:w-[10%] text-xs md:text-sm text-zinc-500">
+              <TableHead className="md:w-[5%] text-xs md:text-sm text-zinc-500">
                 Data
+              </TableHead>
+              <TableHead className="md:w-[5%] text-xs md:text-sm text-zinc-500 ">
+                Deletar
               </TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {transitions &&
-              transitions.map((transition) => (
-                <TableRow key={transition.id}>
+            {transactions &&
+              transactions.map((transaction) => (
+                <TableRow key={transaction.id}>
                   <TableCell className="font-medium text-xs md:text-sm">
-                    {transition.description}
+                    {transaction.description}
                   </TableCell>
                   <TableCell
                     className={`text-left text-xs md:text-sm ${
-                      transition.type === "entrada"
+                      transaction.type === "entrada"
                         ? "text-green-600"
                         : "text-red-500"
                     }`}
                   >
-                    {transition.amount}
+                    {Number(transaction.amount).toFixed(2)}€
                   </TableCell>
                   <TableCell className="text-xs md:text-sm">
-                    {transition.category}
+                    {transaction.category}
                   </TableCell>
                   <TableCell className="text-xs md:text-sm">
-                    {transition.createdAt}
+                    {transaction.createdAt}
+                  </TableCell>
+                  <TableCell className=" flex text-xs md:text-sm content-center justify-center items-center">
+                    <FaRegTrashAlt
+                      onClick={() => handleDelete(transaction.id)}
+                      size={18}
+                      color="red"
+                    />
                   </TableCell>
                 </TableRow>
               ))}
@@ -73,21 +100,28 @@ export function TableMoney() {
         </Table>
       </div>
       <div className="md:hidden w-full max-w-[1120px] items-center  flex flex-col gap-4 mx-auto ">
-        {transitions &&
-          transitions.map((transition) => (
-            <Card className="w-[95%] " key={transition.id}>
+        {transactions &&
+          transactions.map((transaction) => (
+            <Card className="w-[95%] " key={transaction.id}>
               <CardHeader>
-                <CardTitle> {transition.description}</CardTitle>
+                <div className="flex justify-between items-center">
+                  <CardTitle> {transaction.description}</CardTitle>
+                  <FaRegTrashAlt
+                    onClick={() => handleDelete(transaction.id)}
+                    size={18}
+                    color="red"
+                  />
+                </div>
               </CardHeader>
               <CardContent>
                 <p
                   className={`text-left text-sm ${
-                    transition.type === "entrada"
+                    transaction.type === "entrada"
                       ? "text-green-600"
                       : "text-red-500"
                   }`}
                 >
-                  {transition.amount}€
+                  {Number(transaction.amount).toFixed(2)}€
                 </p>
               </CardContent>
               <div className="flex justify-between px-6 mb-5">
@@ -98,7 +132,7 @@ export function TableMoney() {
                     width={20}
                     height={20}
                   ></Image>
-                  <span>{transition.category}</span>
+                  <span>{transaction.category}</span>
                 </CardDescription>
                 <CardDescription className="flex items-center gap-2">
                   <Image
@@ -107,7 +141,7 @@ export function TableMoney() {
                     width={20}
                     height={20}
                   ></Image>
-                  <span>{transition.createdAt}</span>
+                  <span>{transaction.createdAt}</span>
                 </CardDescription>
               </div>
             </Card>
